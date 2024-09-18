fea1d82e68d24e52f3aad32b29614919699b1297
kevin.g <kevingeejo02@gmail.com>
2024-09-18 12:22:13 +0530

    Add project files.

---
+++ DAO/IPersonalDao.cs
@@ -0,0 +1,10 @@
+﻿using PersonalAPI.Models;
+
+namespace PersonalAPI.DAO
+{
+    public interface IPersonalDao
+    {
+        Task<List<PersonalInformationData>> GetAllPersonalInfo(string baseUri);
+        Task<int> InsertPersonalDetails(InsertPersonalDetails personalInfo, string imageName);
+    }
+}
---
+++ Models/Models.cs
@@ -0,0 +1,36 @@
+﻿namespace PersonalAPI.Models
+{
+    public class PersonalInformationData
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string DateOfBirth { get; set; }
+        public string ResidentialAddress { get; set; }
+        public string PermanentAddress { get; set; }
+        public string MobileNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public string MaritalStatus { get; set; }
+        public string Gender { get; set; }
+        public string Occupation { get; set; }
+        public string AadharCardNumber { get; set; }
+        public string PANNumber { get; set; }
+        public string ImageUrl { get; set; }
+    }
+
+
+    public class InsertPersonalDetails
+    {
+        public string Name { get; set; }
+        public string DateOfBirth { get; set; }
+        public string ResidentialAddress { get; set; }
+        public string PermanentAddress { get; set; }
+        public string MobileNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public string MaritalStatus { get; set; }
+        public string Gender { get; set; }
+        public string Occupation { get; set; }
+        public string AadharCardNumber { get; set; }
+        public string PANNumber { get; set; }
+        public IFormFile ImageFile { get; set; }
+    }
+}
